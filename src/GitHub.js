29acc9b4f2335adc6aac1fa04b1276fbbfe31358
senocax <facebookshop@hotmail.com>
2020-09-15 16:38:52 -0300

    async update

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,41 +1,31 @@
-<<<<<<< Codesandbox
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'; // npm install axios
-import ReactLoading from 'react-loading';
-import { Media } from 'react-bootstrap';
-=======
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // npm install axios
 import ReactLoading from "react-loading";
 import { Media } from "react-bootstrap";
->>>>>>> GitHub
 
-<<<<<<< Codesandbox
 function GitHub() {
-
-    const [data, setData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [isLoading, setIsLoading] = useState(false);    
-    const [error, setError] = useState(null);    
-
-    useEffect(() =>{
-        getData();
-    },[])
-=======
-const GitHub = () => {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("https://api.github.com/search/users?q=greg")
-      .then((res) => setData(res.data.items));
-    setIsLoading(false);
->>>>>>> GitHub
+    getData();
   }, []);
 
-  const getData = async () => {};
+  const getData = async () => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${searchTerm}`
+    );
+    setData(res.data.items);
+    setIsLoading(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    getData();
+  };
 
   const listUsers = data.map((user) => (
     <Media key={user.id}>
@@ -54,32 +44,21 @@ const GitHub = () => {
       </Media.Body>
     </Media>
   ));
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
       <h3>GitHub Users Results</h3>
       {isLoading && <ReactLoading type="spinningBubbles" color="#444" />}
       {listUsers}
+      {error && <div className="text-red-font-bold">{error.message}</div>}
     </div>
   );
-};
-
-export default GitHub;
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text"
-                    onChange={event=>setSearchTerm(event.target.value)}
-                />
-                <button type="submit">Search</button>
-            </form>
-            <h3>GitHub Users Results</h3>
-            { isLoading &&                             
-                <ReactLoading type="spinningBubbles" color="#444" />             
-            } 
-            {listUsers} 
-            {error && <div className="text-red-font-bold">{error.message}</div>}                                                                                                   
-        </div>
-    );
 }
+export default GitHub;
