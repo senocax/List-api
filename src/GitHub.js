8ebffbefb1f32289e3e93015484e2759d2cd9e8a
senocax <facebookshop@hotmail.com>
2020-09-15 16:27:13 -0300

    refator async add

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,8 +1,27 @@
+<<<<<<< Codesandbox
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; // npm install axios
+import ReactLoading from 'react-loading';
+import { Media } from 'react-bootstrap';
+=======
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ReactLoading from "react-loading";
 import { Media } from "react-bootstrap";
+>>>>>>> GitHub
 
+<<<<<<< Codesandbox
+function GitHub() {
+
+    const [data, setData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [isLoading, setIsLoading] = useState(false);    
+    const [error, setError] = useState(null);    
+
+    useEffect(() =>{
+        getData();
+    },[])
+=======
 const GitHub = () => {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,6 +32,7 @@ const GitHub = () => {
       .get("https://api.github.com/search/users?q=greg")
       .then((res) => setData(res.data.items));
     setIsLoading(false);
+>>>>>>> GitHub
   }, []);
 
   const getData = async () => {};
@@ -44,3 +64,22 @@ const GitHub = () => {
 };
 
 export default GitHub;
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    onChange={event=>setSearchTerm(event.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+            <h3>GitHub Users Results</h3>
+            { isLoading &&                             
+                <ReactLoading type="spinningBubbles" color="#444" />             
+            } 
+            {listUsers} 
+            {error && <div className="text-red-font-bold">{error.message}</div>}                                                                                                   
+        </div>
+    );
+}
